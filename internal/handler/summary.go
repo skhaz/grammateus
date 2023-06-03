fb735e58a6bdee53e890c60d7f7c087857608904
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-06-03 15:44:45 -0300

    Work in progress

--- internal/handler/summary.go
+++ internal/handler/summary.go
@@ -43,7 +43,7 @@ func (h *Handler) Summary(w http.ResponseWriter, r *http.Request) {
 		Model: openai.ModelGpt35Turbo,
 		Messages: []*openai.Message{
 			{Role: openai.RoleSystem, Content: "You are a streamer assistant who resumes what their chat is about."},
-			{Role: openai.RoleSystem, Content: "Your analysis must be fun and straight to the point."},
+			{Role: openai.RoleSystem, Content: "Your analysis must be fun or ironic and straight to the point."},
 			{Role: openai.RoleSystem, Content: "The result summary must be in the same language as the messages."},
 			{Role: openai.RoleSystem, Content: "Make a summary in one of the following messages enclosed with quotes."},
 			{Role: openai.RoleUser, Content: strings.Join(messages, " ")},
