f2b2ff642ca86f2c046ecb0030f3967a25b9b5b9
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-06-08 18:09:04 -0300

    Work in progress

--- pkg/handler/index.go
+++ pkg/handler/index.go
@@ -5,18 +5,21 @@ import (
 	"net/http"
 )
 
-//go:embed public/index.html
-var index embed.FS
+//go:embed public
+var public embed.FS
 
 func (h *Handler) Index(writer http.ResponseWriter, request *http.Request) {
-	bytes, _ := index.ReadFile("public/index.html")
+	bytes, _ := public.ReadFile("public/index.html")
 
 	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 	writer.Write(bytes)
 }
 
+//go:embed public/favicon.ico
+var favicon embed.FS
+
 func (h *Handler) Icon(writer http.ResponseWriter, request *http.Request) {
-	bytes := make([]byte, 0)
+	bytes, _ := public.ReadFile("public/index.html")
 
 	writer.Header().Set("Content-Type", "image/x-icon")
 	writer.Write(bytes)
