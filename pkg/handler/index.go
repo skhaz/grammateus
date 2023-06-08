773ecc376e213c748a71f35eeafd234406e97e0b
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-06-08 18:22:42 -0300

    Work in progress

--- pkg/handler/index.go
+++ pkg/handler/index.go
@@ -18,6 +18,7 @@ func (h *Handler) Index(writer http.ResponseWriter, request *http.Request) {
 func (h *Handler) Icon(writer http.ResponseWriter, request *http.Request) {
 	bytes, _ := public.ReadFile("public/favicon.ico")
 
+	writer.Header().Set("Cache-Control", "public, max-age=31536000")
 	writer.Header().Set("Content-Type", "image/x-icon")
 	writer.Write(bytes)
 }
